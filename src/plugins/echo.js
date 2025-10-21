7ad3f3a7cad1af9d8237af14a271a378c32d0a70
chrislain avocegan <chrislainavocegan24@gmail.com>
2025-10-21 19:58:43 +0100

    Correction 4

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -1,25 +1,14 @@
-import Echo from "laravel-echo"
-import Pusher from "pusher-js"
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
-window.Pusher = Pusher
+window.Pusher = Pusher;
 
 const echo = new Echo({
   broadcaster: "pusher",
-  key: "83d69067c9d554963de2",
-  cluster: "eu",
+  key: import.meta.env.VITE_PUSHER_APP_KEY,      // ou ta clé directement
+  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER, // ou "mt1"
+  forceTLS: true,     // Toujours true sur prod (HTTPS)
+  encrypted: true,    // Recommandé pour sécuriser la connexion
+});
 
-  wsHost: window.location.hostname, // évite le hardcode localhost
-  wsPort: 6001,
-  wssPort: 6001,
-  forceTLS: false,          // ⚠️ mettre false en local
-  enabledTransports: ["ws", "wss"],
-
-  authEndpoint: "http://localhost:8000/api/broadcasting/auth",
-  auth: {
-    headers: {
-      Accept: "application/json",
-    },
-  },
-})
-
-export default echo
+export default echo;
